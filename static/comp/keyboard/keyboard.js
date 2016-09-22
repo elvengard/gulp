895b9a3036cd49cabd8ab49ded84323d47ea5575
Elvengard <sincemac@gmail.com>
2016-09-22 23:05:43 +0800

    add fis output

---
+++ static/comp/keyboard/keyboard.js
@@ -0,0 +1,7 @@
+define('comp/keyboard/keyboard', function(require, exports, module) {
+
+  console.log("keybord has run");
+  var table = "<tr>\n    <td><%-user%><td>\n    <td>测试1<td>\n    <td>测试1<td>\n    <td>测试1<td>\n    <td>测试1<td>\n<tr>\n";
+  
+
+});
---
+++ static/comp/webpage/index/index.js
@@ -0,0 +1,11 @@
+define('comp/webpage/index/index', function(require, exports, module) {
+
+  var cal = require('comp/cal/cal');
+  console.log('work');
+  // 1 - 0
+  console.log(cal.min(1, 0));
+  
+  var keybord = require('comp/keyboard/keyboard')
+  
+
+});
